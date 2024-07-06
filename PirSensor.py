73b8d0bc925b705a73d4a0dc440037ca09b0b372
Mehrdadeshghi <158504801+Mehrdadeshghi@users.noreply.github.com>
2024-07-06 22:17:04 +0200

    Update PirSensor.py
    added :
    - Logdatei
    -Ignore first 30 sec

--- PirSensor.py
+++ PirSensor.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+from datetime import datetime
 
 # GPIO Pins definieren
 SENSOR_PIN = 25  # Pin für den Bewegungssensor
@@ -10,14 +11,30 @@ GPIO.setmode(GPIO.BCM)
 # GPIO-Pin als Eingang definieren
 GPIO.setup(SENSOR_PIN, GPIO.IN)
 
+# Logdatei öffnen
+log_file_path = "/home/mehrdad/git/Post/logfile.log"
+
+def log_message(message):
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    log_entry = f"{current_time} - {message}"
+    print(log_entry)
+    with open(log_file_path, "a") as log_file:
+        log_file.write(log_entry + "\n")
+
+# Initiale Zeit zum Ignorieren der Bewegungserkennung setzen
+start_time = time.time()
+
 try:
-    print("Warte auf Bewegung...")
+    log_message("Warte auf Bewegung...")
     while True:
-        if GPIO.input(SENSOR_PIN):
-            print("Bewegung erkannt! Brief ist da.")
-        else:
-            print("Keine Bewegung. Kein Brief.")
+        elapsed_time = time.time() - start_time
+        if elapsed_time > 30:  # Ignorieren der Bewegungserkennung für die ersten 30 Sekunden
+            if GPIO.input(SENSOR_PIN):
+                log_message("Bewegung erkannt! Brief ist da.")
+            else:
+                log_message("Keine Bewegung. Kein Brief.")
         time.sleep(1)  # Überprüfen Sie den Sensor jede Sekunde
 except KeyboardInterrupt:
     GPIO.cleanup()
-    print("Programm beendet.")
+    log_message("Programm beendet.")
