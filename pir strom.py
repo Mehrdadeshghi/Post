ca8bc66d013cab42aee5399ddf340189c4a53d42
Mehrdadeshghi <158504801+Mehrdadeshghi@users.noreply.github.com>
2024-07-14 01:45:27 +0200

    Update pir strom.py

--- pir strom.py
+++ pir strom.py
@@ -29,6 +29,8 @@ try:
     log_message("Warte auf Bewegung...")
     last_signal_time = time.time()
     pir_no_power_start_time = None
+    movement_count = 0
+    movement_window_start = time.time()
 
     while True:
         elapsed_time = time.time() - start_time
@@ -37,6 +39,19 @@ try:
                 log_message("Bewegung erkannt! Brief ist da.")
                 last_signal_time = time.time()
                 pir_no_power_start_time = None  # Reset if PIR has power
+                movement_count += 1
+                
+                # Überprüfen, ob mehr als 10 Bewegungen innerhalb von 1 Minute erkannt wurden
+                if time.time() - movement_window_start <= 60:
+                    if movement_count > 10:
+                        log_message("Du hast Post")
+                        movement_count = 0  # Reset count after notification
+                        movement_window_start = time.time()  # Reset the window
+                else:
+                    # Reset the count and window if more than 1 minute has passed
+                    movement_count = 1
+                    movement_window_start = time.time()
+                    
             else:
                 # Check if the PIR sensor has no power
                 if pir_no_power_start_time is None:
