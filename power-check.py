25f3791fc0682c7c84864e89cd5b597f5accd36a
Mehrdadeshghi <158504801+Mehrdadeshghi@users.noreply.github.com>
2024-07-18 00:35:15 +0200

    Update power-check.py

--- power-check.py
+++ power-check.py
@@ -1,22 +1,27 @@
-from gpiozero import InputDevice
-import time
+import busio
+import digitalio
+import board
+from adafruit_mcp3xxx.mcp3008 import MCP3008
+from adafruit_mcp3xxx.analog_in import AnalogIn
 
-# Der GPIO-Pin, an den der PIR-Sensor angeschlossen ist
-power_pin = 25
+# SPI-Bus initialisieren
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+cs = digitalio.DigitalInOut(board.D5)  # Chip select des ADC
+mcp = MCP3008(spi, cs)
 
-# Erstellen eines InputDevice, um den Stromstatus zu lesen
-sensor_power = InputDevice(power_pin)
+# Analogeingangspins erstellen
+power_monitor = AnalogIn(mcp, MCP3008.P0)  # An welchem Pin des ADC der Spannungsteiler angeschlossen ist
+
+def get_voltage(analog_input):
+    return (analog_input.voltage * 3.3) / 4096
 
 try:
     while True:
-        if sensor_power.is_active:
-            print("PIR-Sensor hat Strom.")
+        voltage = get_voltage(power_monitor)
+        if voltage > 1.0:  # Grenze einstellen, ab wann man annimmt, dass der PIR-Sensor Strom hat
+            print("PIR-Sensor ist eingeschaltet.")
         else:
-            print("PIR-Sensor hat keinen Strom.")
-        
-        # Pause für 1 Sekunde, um das Lesen zu verlangsamen
+            print("PIR-Sensor ist ausgeschaltet.")
         time.sleep(1)
-
 except KeyboardInterrupt:
     print("Programm wurde vom Benutzer gestoppt.")
-
