ea2816ddd48df4ea7c2bb5f54de1311ad648cb17
Mehrdadeshghi <158504801+Mehrdadeshghi@users.noreply.github.com>
2024-07-18 00:32:52 +0200

    Create power-check.py

---
+++ power-check.py
@@ -0,0 +1,22 @@
+from gpiozero import InputDevice
+import time
+
+# Der GPIO-Pin, an den der PIR-Sensor angeschlossen ist
+power_pin = 17
+
+# Erstellen eines InputDevice, um den Stromstatus zu lesen
+sensor_power = InputDevice(power_pin)
+
+try:
+    while True:
+        if sensor_power.is_active:
+            print("PIR-Sensor hat Strom.")
+        else:
+            print("PIR-Sensor hat keinen Strom.")
+        
+        # Pause für 1 Sekunde, um das Lesen zu verlangsamen
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Programm wurde vom Benutzer gestoppt.")
+
