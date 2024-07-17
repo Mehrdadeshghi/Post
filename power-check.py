5ef5af02cef83d6810cdc824c75f137cd1d5ef2e
Mehrdadeshghi <158504801+Mehrdadeshghi@users.noreply.github.com>
2024-07-18 00:37:16 +0200

    Update power-check.py

--- power-check.py
+++ power-check.py
@@ -1,27 +1,24 @@
-import busio
-import digitalio
-import board
-from adafruit_mcp3xxx.mcp3008 import MCP3008
-from adafruit_mcp3xxx.analog_in import AnalogIn
+import RPi.GPIO as GPIO
+import time
 
-# SPI-Bus initialisieren
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-cs = digitalio.DigitalInOut(board.D5)  # Chip select des ADC
-mcp = MCP3008(spi, cs)
+# Pin-Nummerierung nach BCM verwenden
+GPIO.setmode(GPIO.BCM)
 
-# Analogeingangspins erstellen
-power_monitor = AnalogIn(mcp, MCP3008.P0)  # An welchem Pin des ADC der Spannungsteiler angeschlossen ist
-
-def get_voltage(analog_input):
-    return (analog_input.voltage * 3.3) / 4096
+# Pin 2 als Eingang festlegen
+pin_number = 2
+GPIO.setup(pin_number, GPIO.IN)
 
 try:
     while True:
-        voltage = get_voltage(power_monitor)
-        if voltage > 1.0:  # Grenze einstellen, ab wann man annimmt, dass der PIR-Sensor Strom hat
-            print("PIR-Sensor ist eingeschaltet.")
+        # Den Status des Pins lesen
+        if GPIO.input(pin_number) == GPIO.HIGH:
+            print("Pin 2 ist HIGH - Strom wird gezogen.")
         else:
-            print("PIR-Sensor ist ausgeschaltet.")
+            print("Pin 2 ist LOW - Kein Strom wird gezogen.")
+
+        # Um das Lesen zu verlangsamen
         time.sleep(1)
+
 except KeyboardInterrupt:
     print("Programm wurde vom Benutzer gestoppt.")
+    GPIO.cleanup()
