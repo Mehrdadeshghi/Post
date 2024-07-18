909f2b970368c525cf7a549f52200fb80351f381
Mehrdadeshghi <158504801+Mehrdadeshghi@users.noreply.github.com>
2024-07-19 01:11:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 from sensor import sensor_bp
 from management import management_bp
 
 app = Flask(__name__)
 
-# Registrierung der Blueprints mit ihren URL-Präfixen
 app.register_blueprint(sensor_bp, url_prefix='/sensor')
 app.register_blueprint(management_bp, url_prefix='/management')
 
