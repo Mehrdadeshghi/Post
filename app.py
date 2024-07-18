82901ea48a46e0c9d86f3398b1eebea97fd184c4
Mehrdadeshghi <158504801+Mehrdadeshghi@users.noreply.github.com>
2024-07-19 00:34:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import time
 import os
 import psutil
 from datetime import datetime, timedelta
-from flask import Flask, jsonify, render_template, send_file
+from flask import Flask, jsonify, render_template, send_file, url_for
 import pandas as pd
 import io
 
