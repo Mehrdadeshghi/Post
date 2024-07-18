60fb3c493524505b2b131c2a8bd7a6113ec190e4
Mehrdadeshghi <158504801+Mehrdadeshghi@users.noreply.github.com>
2024-07-19 01:08:03 +0200

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,21 +1,45 @@
 import RPi.GPIO as GPIO
 import time
-from flask import Blueprint, render_template
+from flask import Blueprint, jsonify
+from datetime import datetime
 
 sensor_bp = Blueprint('sensor', __name__)
 
-# Setzen der GPIO-Pins
 SENSOR_PIN = 25
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SENSOR_PIN, GPIO.IN)
 
-@sensor_bp.route('/')
-def sensor_dashboard():
-    return render_template('sensor.html')
+# Globale Liste zur Speicherung der Bewegungszeiten
+movements = []
 
-def check_sensor():
-    """Dieser Funktion würde in einem echten Szenario in einem separaten Thread laufen."""
-    while True:
-        if GPIO.input(SENSOR_PIN):
-            print("Bewegung erkannt!")
-        time.sleep(1)
+def detect_movement():
+    """Diese Funktion wird bei jeder Bewegung aufgerufen."""
+    movement_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    movements.append({"time": movement_time})
+    print(f"Bewegung erkannt um {movement_time}")
+
+# Callback-Funktion zur Bewegungserkennung
+def motion_detected(channel):
+    detect_movement()
+
+GPIO.add_event_detect(SENSOR_PIN, GPIO.RISING, callback=motion_detected)
+
+@sensor_bp.route('/movements')
+def movements_list():
+    """Liefert die Liste aller erkannten Bewegungen."""
+    return jsonify(movements)
+
+@sensor_bp.route('/status')
+def status():
+    """Liefert den aktuellen Status des Sensors."""
+    last_movement = movements[-1]['time'] if movements else 'Keine Bewegungen erkannt'
+    return jsonify({"status": "Aktiv", "last_movement": last_movement})
+
+@sensor_bp.route('/summary')
+def summary():
+    """Gibt eine Zusammenfassung der Bewegungen zurück."""
+    total_movements = len(movements)
+    return jsonify({
+        "total_movements": total_movements,
+        "last_movement": movements[-1]['time'] if movements else 'Keine Bewegungen erkannt'
+    })
