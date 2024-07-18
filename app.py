8390302512be26aabf2d2e13889c4bd0ba3a9a5b
Mehrdadeshghi <158504801+Mehrdadeshghi@users.noreply.github.com>
2024-07-19 01:05:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from sensor import sensor_bp
 from management import management_bp
 
 app = Flask(__name__)
+
 app.register_blueprint(sensor_bp, url_prefix='/sensor')
 app.register_blueprint(management_bp, url_prefix='/management')
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
