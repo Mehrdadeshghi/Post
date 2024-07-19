db5f0114db70ebf2d4d3dafa6dfc8c7b29e3f7c5
Mehrdadeshghi <158504801+Mehrdadeshghi@users.noreply.github.com>
2024-07-19 20:26:13 +0200

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS Buildings (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    city VARCHAR(100),
+    postal_code VARCHAR(10),
+    street VARCHAR(100),
+    house_number VARCHAR(10),
+    hostname VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS PIR_Sensors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    rp_hostname VARCHAR(50),
+    sensor_number INT,
+    postbox_number VARCHAR(50),
+    FOREIGN KEY (rp_hostname) REFERENCES Buildings(hostname)
+);
+
+CREATE TABLE IF NOT EXISTS Ports (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    hostname VARCHAR(50),
+    port INT,
+    state VARCHAR(10),
+    FOREIGN KEY (hostname) REFERENCES Buildings(hostname)
+);
