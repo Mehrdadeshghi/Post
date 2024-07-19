58dd18ab86394096f1c1d8218dacae1621d10557
Mehrdadeshghi <158504801+Mehrdadeshghi@users.noreply.github.com>
2024-07-19 11:40:56 +0200

    Update and rename app.py to server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 import RPi.GPIO as GPIO
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 # Setup GPIO
 GPIO.setmode(GPIO.BCM)
@@ -16,6 +16,10 @@ def get_pin_states():
     for pin in pins:
         states[pin] = GPIO.input(pin)
     return jsonify(states)
+
+@app.route('/')
+def index():
+    return send_from_directory('static', 'index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
