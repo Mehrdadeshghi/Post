722ff5b3e6ed18b1e7dd54e673b2aab3a0230ef5
Mehrdadeshghi <158504801+Mehrdadeshghi@users.noreply.github.com>
2024-07-19 13:09:41 +0200

    Create dbtest.py

---
+++ dbtest.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return "Hello World!"
+
+def setup_database(app):
+    with app.app_context():
+        db.create_all()
+
+if __name__ == '__main__':
+    setup_database(app)
+    app.run(debug=True)
