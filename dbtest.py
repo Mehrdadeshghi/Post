7798f01ae925294a52489897d1ca5dbecf546d25
Mehrdadeshghi <158504801+Mehrdadeshghi@users.noreply.github.com>
2024-07-19 13:11:54 +0200

    Update dbtest.py

--- dbtest.py
+++ dbtest.py
@@ -8,12 +8,12 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return "Hello, world!"
 
 def setup_database(app):
     with app.app_context():
         db.create_all()
 
 if __name__ == '__main__':
-    setup_database(app)
-    app.run(debug=True)
+    setup_database(app)  # Setze die Datenbank auf, bevor der Server läuft
+    app.run(host='0.0.0.0', port=5001, debug=True)  # Flask App auf Port 5001 starten
