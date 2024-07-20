789e10c054181c1b1222b809f8b48aab22df299f
Mehrdadeshghi <158504801+Mehrdadeshghi@users.noreply.github.com>
2024-07-20 18:58:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -13,6 +13,10 @@ def log_motion():
 @app.route('/motion', methods=['GET'])
 def get_motion():
     return jsonify(motion_data), 200
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
