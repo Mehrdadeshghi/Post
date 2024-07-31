cda2534dec264aacddd286ac489ef353bc397786
Mehrdadeshghi <158504801+Mehrdadeshghi@users.noreply.github.com>
2024-07-31 19:55:28 +0200

    Update seven.py

--- seven.py
+++ seven.py
@@ -1,5 +1,4 @@
 import RPi.GPIO as GPIO
-import time
 
 # GPIO-Modus (BCM)
 GPIO.setmode(GPIO.BCM)
@@ -7,33 +6,13 @@ GPIO.setmode(GPIO.BCM)
 # Definition der GPIO-Pins für die Segmente (A-G und DP)
 segments = (17, 18, 27, 22, 23, 12, 16, 4)  # Reihenfolge: A, B, C, D, E, F, G, DP
 
-# Segmente als Ausgang setzen
+# Segmente als Ausgang setzen und auf HIGH (1) schalten
 for segment in segments:
     GPIO.setup(segment, GPIO.OUT)
-    GPIO.output(segment, 0)
-
-# Segment-Muster für Ziffern definieren
-num = {' ': (0, 0, 0, 0, 0, 0, 0),
-       '0': (1, 1, 1, 1, 1, 1, 0),
-       '1': (0, 1, 1, 0, 0, 0, 0),
-       '2': (1, 1, 0, 1, 1, 0, 1),
-       '3': (1, 1, 1, 1, 0, 0, 1),
-       '4': (0, 1, 1, 0, 0, 1, 1),
-       '5': (1, 0, 1, 1, 0, 1, 1),
-       '6': (1, 0, 1, 1, 1, 1, 1),
-       '7': (1, 1, 1, 0, 0, 0, 0),
-       '8': (1, 1, 1, 1, 1, 1, 1),
-       '9': (1, 1, 1, 1, 0, 1, 1)}
-
-# Funktion zur Anzeige einer Ziffer auf dem 1-stelligen Display
-def display_number(number):
-    for loop in range(0, 7):
-        GPIO.output(segments[loop], num[number][loop])
+    GPIO.output(segment, GPIO.HIGH)  # Schaltet alle Segmente dauerhaft ein
 
 try:
     while True:
-        for i in range(10):
-            display_number(str(i))
-            time.sleep(1)
+        pass  # Endlosschleife, um das Programm am Laufen zu halten
 except KeyboardInterrupt:
-    GPIO.cleanup()
+    GPIO.cleanup()  # GPIO-Pins bereinigen, wenn das Programm mit Strg+C beendet wird
