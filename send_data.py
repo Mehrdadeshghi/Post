aa7ea9c17abb7838f109e7ccabbfdc8d801f529d
Mehrdadeshghi <158504801+Mehrdadeshghi@users.noreply.github.com>
2024-08-04 01:46:58 +0200

    Update send_data.py

--- send_data.py
+++ send_data.py
@@ -1,31 +1,35 @@
-from influxdb_client import InfluxDBClient, Point
-from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb import InfluxDBClient
 import random
 import time
 
 # Details für den InfluxDB-Server
-url = "http://45.149.78.188:8086"
+url = "45.149.78.188"
+port = 8086
 username = "myuser"
 password = "mypassword"
-bucket = "mydatabase"
+database = "mydatabase"
 
-# Erstellen der Verbindung
-client = InfluxDBClient(url=url, username=username, password=password, org="")
-
-# API für das Schreiben von Daten
-write_api = client.write_api(write_options=SYNCHRONOUS)
+client = InfluxDBClient(url, port, username, password, database)
 
 while True:
     # Simulierte Sensordaten
     temperature = random.uniform(20.0, 25.0)
     humidity = random.uniform(30.0, 50.0)
 
-    point = Point("environment") \
-        .tag("location", "RaspberryPi") \
-        .field("temperature", temperature) \
-        .field("humidity", humidity)
+    json_body = [
+        {
+            "measurement": "environment",
+            "tags": {
+                "location": "RaspberryPi"
+            },
+            "fields": {
+                "temperature": temperature,
+                "humidity": humidity
+            }
+        }
+    ]
 
-    write_api.write(bucket=bucket, org="", record=point)
+    client.write_points(json_body)
     print(f"Gesendete Daten: Temperatur={temperature}, Feuchtigkeit={humidity}")
     
     time.sleep(10)  # Daten alle 10 Sekunden senden
