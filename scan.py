ec27b84789cd5c5242e46895c68cca2da63710fe
Mehrdadeshghi <158504801+Mehrdadeshghi@users.noreply.github.com>
2024-08-04 23:11:45 +0200

    Update scan.py

--- scan.py
+++ scan.py
@@ -12,13 +12,12 @@ pins = [2, 3, 4, 17, 27, 22, 10, 9, 11, 5, 6, 13, 19, 26, 21, 20, 16, 12, 7, 8, 
 def scan_pins():
     pin_status = {}
     for pin in pins:
-        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_OFF)
-        try:
-            GPIO.setup(pin, GPIO.OUT, initial=GPIO.HIGH)
-            pin_status[pin] = 'Not Connected'
-        except:
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        status = GPIO.input(pin)
+        if status == GPIO.LOW:
             pin_status[pin] = 'Connected'
-        GPIO.setup(pin, GPIO.IN)
+        else:
+            pin_status[pin] = 'Not Connected'
     return pin_status
 
 @app.route('/')
