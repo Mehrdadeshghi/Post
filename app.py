34f99e55eebcefa0e59c5892033132bf89c1b8e8
Mehrdadeshghi <158504801+Mehrdadeshghi@users.noreply.github.com>
2024-08-08 20:46:54 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from config import Config
+from auth.routes import auth_bp
+from management.routes import management_bp
+from user.routes import user_bp
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Registriere die Blueprints
+app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(management_bp, url_prefix='/management')
+app.register_blueprint(user_bp, url_prefix='/user')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
